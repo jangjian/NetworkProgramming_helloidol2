72ce1ebcb0525310d68f8c7606bc9cf89a6dd177
Jian Jang <pureunnabi@gmail.com>
2024-05-13 12:16:33 +0900

    views CharacterListView

--- t1/views.py
+++ t1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from t1.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # character_list = Character.object.all() #DB에 모델 데이터 다 가져오자
+    # return render(request 't1/character_list.html', context={'character_list' : character_list})
+    # #모델_list.html에 모델_list라는 키로 DB에서 가져온 데이터 넣어서 render한다.
